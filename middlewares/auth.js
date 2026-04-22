a813170f3edb594c4d0047b0cfd9247fdc3bffd6
Rodrigo Dominguez <licrodrigo3x3@gmail.com>
2026-04-22 10:37:54 -0600

    proteccion del link

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,9 @@
 module.exports = (req, res, next) => {
-    if (!req.session.user) {
+    // Si no hay sesión o no hay objeto de usuario en ella
+    if (!req.session || !req.session.user) {
+        // Limpiamos cualquier rastro de sesión inválida
         return res.redirect('/login');
     }
+    // Si todo está bien, permitimos el acceso
     next();
 };
